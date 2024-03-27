74170e84c2620712c6074e6ceeb211267416b318
Serhii Trachuk <105816939+dziubimaru@users.noreply.github.com>
2024-03-27 21:40:17 +0100

    Started working on FeatureViewModel

---
+++ Globomantics.Windows/ViewModels/FeatureViewModel.cs
@@ -0,0 +1,38 @@
+﻿using Globomantics.Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Globomantics.Infrastructure.Data.Repositories;
+using CommunityToolkit.Mvvm.Input;
+
+namespace Globomantics.Windows.ViewModels
+{
+    public class FeatureViewModel : BaseTodoViewModel<Feature>
+    {
+        private readonly IRepository<Feature> repository;
+
+        private string? description;
+
+        public string? Description 
+        { 
+            get => description;
+            set
+            {
+                description = value;
+                OnPropertyChanged(nameof(Description));
+            }
+        }
+
+        public FeatureViewModel(IRepository<Feature> repository) : base()
+        {
+            this.repository = repository;
+            SaveCommand = new RelayCommand(async () => await SaveAsync());
+        }
+        public override Task SaveAsync()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
